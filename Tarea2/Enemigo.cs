5b1341c39f12199c88ebd6f532eae9411d9f92a2
Ðavid <avid@DESKTOP-EFSC9KH>
2018-08-15 21:52:25 -0700

    Add project files.

---
+++ Tarea2/Enemigo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea2
+{
+    class Enemigo
+    {
+        public int vida = 10;
+        public int x = 1;
+        public int y = 1;
+        public int puntos = 100;
+        public string moverse = "se mueve";
+        public string disparar = "dispara";
+        public string explotar = "explota";
+    }
+}
---
+++ Tarea2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Enemigo enemigo = new Enemigo();
+            Random r = new Random();
+            int a = r.Next(1, 4);
+            do
+            {
+                a = r.Next(1, 4);
+                if (a == 1)
+                {
+                    Console.WriteLine("El enemigo " + enemigo.moverse);
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+                if (a == 2)
+                {
+                    Console.WriteLine("El enemigo " + enemigo.disparar);
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+                if (a == 3)
+                {
+                    Console.WriteLine("El enemigo " + enemigo.explotar);
+                    Console.WriteLine("puntos: " + enemigo.puntos);
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+            } while (a != 3);
+        }  
+    }
+}
